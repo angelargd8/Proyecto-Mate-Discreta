853e3c783b1968b81ab349c4294e78fa78d62edb
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2023-11-24 10:51:25 -0600

    desencriptar jkk

--- Proyecto.py
+++ Proyecto.py
@@ -20,10 +20,13 @@ import csv
 menu = True
 os.system("cls") # limpia la consola
 
+#--- diccionario ---
+diccionario= {chr(i + ord('A')): i for i in range(26)}
+
 #--- funciones ---
 
 def MostrarMenu():
-    print("\n\n Bienvenido alsistema RSA, ingrese una opción: \n1. Encriptar Mensaje\n2. Desencriptar Mensaje\n3. Salir")
+    print("\n\nBienvenido al sistema RSA, ingrese una opción: \n1. Encriptar Mensaje\n2. Desencriptar Mensaje\n3. Salir")
 
 
 
@@ -52,15 +55,97 @@ la llave privada 𝑑 y desencriptar el mensaje
 
 def desencriptacion():
     #pedir datos
-    C = int(input("Ingrese el valor de C (mensaje encriptado): "))
+    C = str(input("Ingrese el valor de C (mensaje encriptado) sin espacios en bloques de 4: "))
     n = int(input("Ingrese el valor de n: "))
     e = int(input("Ingrese el valor de e: "))
 
-    #encontrar la llave privada
+    if( n>0 and e>0 ):
+        #---1. encontrar la llave privada----
 
-    #desencriptar el mensaje
+        #calcular los valores de p y q (factorizar n)
+        lista_primos= factorizar(n)
+        p = lista_primos[0]
+        q = lista_primos[1]
+        #print(p,q)
 
-    #eee sigo mañana xd
+        #funcion de euler
+        fi = (p-1)*(q-1)
+        #print(fi)
+
+        #encontrar la llave privada
+        #numero que e*d sea congruente a 1 y mcd(fi)
+        d= encontrarD(e, fi)
+        print("la llave privada es: "+str(d)+","+str(n))
+
+        #----2. desencriptar el mensaje-----
+        #separar el mensaje en bloques de 4
+        lista_mensaje = []
+        for i in range(0, len(C), 4):
+            lista_mensaje.append(C[i:i+4])
+        #print(str(lista_mensaje))
+
+        #print(diccionario)
+        mensaje=[]
+
+        for i in lista_mensaje:
+            i = int(i)
+            m = (i**(d)) % n
+            #convertir m a 2 strings y luego compararlo con el diccionario
+            strM= str(m)
+            #print(strM)
+            mitad= len(strM)//2
+            mitad1= int(strM[:mitad])
+            mitad2= int(strM[mitad:])
+            #print("primera mitad: "+str(mitad1))
+            #print("segunda mitad: "+str(mitad2))
+            mensaje.append(buscarLlave(mitad1) + buscarLlave(mitad2))
+
+        #3. mostrar el mensaje desencriptado
+        mensajeFinal= ''.join(mensaje)
+        print("el mensaje es: "+str(mensajeFinal))
+    else:
+        print("ingrese numeros enteros positivos")
+
+    
+    
+
+#buscar llave
+def buscarLlave(valor):
+    for llave, value in diccionario.items():
+        if value == valor:
+            return llave
+    return "No existe la llave" #jjk anque no debería de pasar porque como es mod 26
+
+#maximo comun divisor
+def mcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        MCD, x, y = mcd(b % a, a)
+        return MCD, y - (b // a) * x, x
+    
+
+def encontrarD(e, fi):
+    MCD, x, y = mcd(e, fi)
+    if MCD != 1: 
+        print("No se puede encontrar la llave privada")
+    else: 
+        return x % fi
+
+#factorizar n
+def factorizar(n):
+    primo = 2 #primer numero primo
+    factores = []
+    while primo * primo <= n: #es primo^2 porque se bucan factores hasta sqrt(n)
+        if n % primo:
+            primo += 1
+        else:
+            #comprobar que sea divisible
+            n = n// primo 
+            factores.append(primo)
+    if n > 1:
+        factores.append(n)
+    return factores
 
 
 #---- menu principal ----
@@ -69,14 +154,14 @@ try:
         MostrarMenu()
         opcion = int(input("Ingrese la opción: "))
         if opcion == 1: 
+            #print(diccionario)
             encriptacion()
         elif opcion == 2:
             desencriptacion()
-        elif opcion > 2:
+        elif opcion == 3:
             break
         else: 
             print("esta opción no es válida")
 except:
     print("Esta opción no es válida, vuelva a intentarlo con los datos correctos \n\n")
-    MostrarMenu() 
 input("pulse una tecla para continuar... ")
